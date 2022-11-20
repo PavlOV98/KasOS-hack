8feb5f59af62872392b55f3e90571cf7dd2d3b31
PavlOV98 <pavl98.o.v@gmail.com>
2022-11-20 15:31:38 +0300

    cas upload

---
+++ KAS/subscriber/src/gpio.cpp
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <gpio/gpio.h>
+#include <stdbool.h>
+#include <string>
+
+
+#ifdef __arm__
+#include <bsp/bsp.h>
+#endif
+
+#define GPIO_PIN_NUM_IN2 12U
+#define GPIO_PIN_NUM_IN1 13U
+#define GPIO_PIN_NUM_ENA 6U
+#define GPIO_PIN_NUM_IN4 20U
+#define GPIO_PIN_NUM_IN3 21U
+#define GPIO_PIN_NUM_ENB 26U
+#define DELAY_S 2
+
+#define BUF_SIZE 1024
+#define HIGH 1
+#define LOW 0
+
+
+
+void forward(GpioHandle* handle){
+    fprintf(stderr, "forward\n");
+     GpioOut(*handle, GPIO_PIN_NUM_IN1, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_IN2, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN3, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN4, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_ENA, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_ENB, HIGH);
+
+}
+
+void stop(GpioHandle* handle) {
+    GpioOut(*handle, GPIO_PIN_NUM_IN1, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN2, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN3, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN4, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_ENA, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_ENB, LOW);
+}
+
+void backward(GpioHandle* handle) {
+    fprintf(stderr, "backward\n");
+    GpioOut(*handle, GPIO_PIN_NUM_IN1, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN2, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_IN3, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_IN4, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_ENA, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_ENB, HIGH);
+}
+
+void left(GpioHandle* handle) {
+    fprintf(stderr, "left\n");
+      GpioOut(*handle, GPIO_PIN_NUM_IN1, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_IN2, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN3, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_IN4, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_ENA, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_ENB, HIGH);
+}
+
+void right(GpioHandle* handle) {
+    fprintf(stderr, "right\n");
+    GpioOut(*handle, GPIO_PIN_NUM_IN1, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN2, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_IN3, LOW);
+    GpioOut(*handle, GPIO_PIN_NUM_IN4, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_ENA, HIGH);
+    GpioOut(*handle, GPIO_PIN_NUM_ENB, HIGH);
+
+}
+
+void trajectory(GpioHandle* handle) {
+    forward(handle);
+    sleep(3);
+    usleep(100000);
+    left(handle);
+    usleep(350000);
+    forward(handle);
+    sleep(1);
+    usleep(700000);
+    left(handle);
+    usleep(350000);
+    forward(handle);
+    sleep(3);
+    usleep(100000);
+    left(handle);
+    usleep(350000);
+    forward(handle);
+    usleep(700000);
+    stop(handle);
+}
+
+
+
+int gpiomain(const std::string& cmd)
+{
+    fprintf(stderr, "Start GPIO_output test\n");
+
+#ifdef __arm__
+    /**
+     * InitializeGPIO_PIN_NUM board support package (BSP) driver and set configuration
+     * for GPIO pins. It`s required for stdout by UART.
+     */
+    {
+        BspError rc = BspInit(NULL);
+        if (rc != BSP_EOK)
+        {
+            fprintf(stderr, "Failed to initialize BSP\n");
+            return EXIT_FAILURE;
+        }
+
+        rc = BspSetConfig("gpio0", "raspberry_pi4b.default");
+        if (rc != BSP_EOK)
+        {
+            fprintf(stderr, "Failed to set mux configuration for gpio0 module\n");
+            return EXIT_FAILURE;
+        }
+    }
+#endif
+
+    if (GpioInit())
+    {
+        fprintf(stderr, "GpioInit failed\n");
+        return EXIT_FAILURE;
+    }
+
+    /* initialize and setup gpio0 port */
+    GpioHandle handle = NULL;
+    if (GpioOpenPort("gpio0", &handle) || handle == GPIO_INVALID_HANDLE)
+    {
+        fprintf(stderr, "GpioOpenPort failed\n");
+        return EXIT_FAILURE;
+    }
+
+    GpioSetMode(handle, GPIO_PIN_NUM_IN1, GPIO_DIR_OUT);
+    GpioSetMode(handle, GPIO_PIN_NUM_IN2, GPIO_DIR_OUT);
+    GpioSetMode(handle, GPIO_PIN_NUM_IN3, GPIO_DIR_OUT);
+    GpioSetMode(handle, GPIO_PIN_NUM_IN4, GPIO_DIR_OUT);
+    GpioSetMode(handle, GPIO_PIN_NUM_ENA, GPIO_DIR_OUT);
+    GpioSetMode(handle, GPIO_PIN_NUM_ENB, GPIO_DIR_OUT);
+
+    fprintf(stderr, "Starting move\n");
+    GpioHandle* p_handle = &handle;
+
+    if(cmd=="move")
+    {
+      forward(p_handle);
+      sleep(1);
+      stop(p_handle);
+
+    }
+    if(cmd=="left")
+    {
+      left(p_handle);
+      sleep(1);
+      stop(p_handle);
+    }
+    if(cmd=="right")
+    {
+      right(p_handle);
+      sleep(1);
+      stop(p_handle);
+    }
+    if(cmd=="stop")
+    {
+      stop(p_handle);
+    }
+    else if (cmd=="box") {
+      trajectory(p_handle);
+    }
+
+
+    if(GpioClosePort(handle))
+    {
+        fprintf(stderr, "GpioClosePort failed\n");
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stderr, "Test finished.\n");
+
+    return EXIT_SUCCESS;
+}
